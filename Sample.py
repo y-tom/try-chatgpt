5e89acaff02383180bc2d6b3ea57d31a27a85ce1
y-tom <47276983+y-tom@users.noreply.github.com>
2023-05-12 09:44:03 +0900

    Initial commit

---
+++ Sample.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+from llama_index.indices.vector_store import GPTVectorStoreIndex
+from llama_index import download_loader,LLMPredictor
+from langchain.chat_models import ChatOpenAI
+
+# Windows 11 Pro 22H2
+# Python 3.11.1
+# pip 22.3.1
+# openai 0.27.6
+# langchain 0.0.158
+
+# APIKeyは環境変数OPENAI_API_KEYにセットしておく
+
+# インプットとなるcsvを用意する
+csvfile = Path('D:\Work\Projects\ChatGPT\sample.csv')
+
+SimpleCSVReader = download_loader("SimpleCSVReader")
+loader = SimpleCSVReader()
+documents = loader.load_data(file=csvfile)
+
+llm_predictor = LLMPredictor(
+    llm=ChatOpenAI(
+        temperature=0,
+        model_name="gpt-3.5-turbo"
+    )
+)
+
+index = GPTVectorStoreIndex.from_documents(
+    documents,
+    llm_predictor=llm_predictor
+)
+
+query_engine = index.as_query_engine()
+# 質問を入力する
+comment = query_engine.query("chatABCの特徴は？")
+print(comment)
